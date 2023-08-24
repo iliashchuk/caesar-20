2bc8a6140ee993ce8f16d47447ccbed126fe223c
Stanislav Iliashchuk <s.iliashchuk@gmail.com>
2023-08-24 15:56:17 +0300

    fix collision detection

--- web/src/static/circleCollision.js
+++ web/src/static/circleCollision.js
@@ -7,8 +7,8 @@ export function createCircleCollisionDetectionForRadius(radius) {
         var circle1 = { radius, x: entry.left, y: entry.top };
         var circle2 = { radius, x: target.left, y: target.top };
 
-        var dx = circle1.x - circle2.x - radius;
-        var dy = circle1.y - circle2.y - radius;
+        var dx = circle1.x - circle2.x;
+        var dy = circle1.y - circle2.y;
         var distance = Math.sqrt(dx * dx + dy * dy);
 
         if (distance < circle1.radius + circle2.radius) {
